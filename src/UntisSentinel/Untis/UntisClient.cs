8dbb44d4579ac646dde988ce37d65075a12d3fc7
llarndorfer <l.larndorfer@students.htl-leonding.ac.at>
2026-09-26 11:42:37 +0200

    feat: implement UntisClient with generic JSON-RPC call

--- src/UntisSentinel/Untis/UntisClient.cs
+++ src/UntisSentinel/Untis/UntisClient.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 using Microsoft.Extensions.Options;
@@ -6,15 +8,41 @@ namespace UntisSentinel.Untis;
 
 public sealed class UntisClient
 {
-    private HttpClient _httpClient;
-    private IOptions<UntisOptions> _options;
+    private readonly HttpClient _httpClient;
+    private readonly UntisOptions _options;
+    private readonly string _baseUrl;
+
+    // JsonSerializerDefaults.Web for camel case
+    private static readonly JsonSerializerOptions SerializerOptions = new(JsonSerializerDefaults.Web);
 
     public UntisClient(HttpClient httpClient, IOptions<UntisOptions> options)
     {
         _httpClient = httpClient;
-        _options = options;
+        _options = options.Value;
+        _baseUrl = $"https://{_options.Host}/WebUntis/jsonrpc.do?school={_options.School}";
     }
 
-    // JsonSerializerDefaults.Web for camel case
-    private static readonly JsonSerializerOptions JsonSerializerOptions = new(JsonSerializerDefaults.Web);
+    private async Task<TResult> CallAsync<TParams, TResult>(string method, TParams parameters, CancellationToken cancellationToken)
+    {
+        JsonRpcRequest<TParams> request = new("1", method, parameters);
+
+        using HttpResponseMessage response =
+        await _httpClient.PostAsJsonAsync(_baseUrl,
+        request,
+        SerializerOptions,
+        cancellationToken);
+
+        // throws on 4xx or 5xx
+        response.EnsureSuccessStatusCode();
+
+        JsonRpcResponse<TResult>? content = await response.Content.ReadFromJsonAsync<JsonRpcResponse<TResult>>(SerializerOptions, cancellationToken)
+        ?? throw new UntisClientException("Content is null");
+        if (content.Error != null)
+        {
+            throw new UntisClientException(content.Error.Code, content.Error.Message);
+        }
+
+        return content.Result ?? throw new UntisClientException("No result nor error exist");
+    }
 }
+
---
+++ src/UntisSentinel/Untis/UntisClientException.cs
@@ -0,0 +1,13 @@
+namespace UntisSentinel.Untis;
+public sealed class UntisClientException : System.Exception
+{
+    public int? Code { get; }
+
+    public UntisClientException(string message) : base(message) { }
+    public UntisClientException(int code, string message) : base($"{message}; (error code: {code})")
+    {
+        Code = code;
+    }
+
+
+}
